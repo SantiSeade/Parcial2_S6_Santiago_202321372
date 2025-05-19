570a5c4f719e51609a62c55be2338540e0def674
FirstName LastName <s.seade@uniandes.edu.co>
2025-05-19 16:58:32 -0500

    Ultimo commit

--- src/app/aerolinea/aerolinea.module.ts
+++ src/app/aerolinea/aerolinea.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AerolineaListComponent } from './aerolinea-list/aerolinea-list.component';
+import { AerolineaDetailComponent } from './aerolinea-detail/aerolinea-detail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [AerolineaListComponent],
-  declarations: [AerolineaListComponent]
+  declarations: [AerolineaListComponent, AerolineaDetailComponent]
 })
 export class AerolineaModule { }
